3b13b6795f739aab1efefaf21da635e854f226d5
OharaKohei <gofue0723@gmail.com>
2016-04-20 14:32:22 +0900

    [add] no.2 and no.3

--- nlp100.py
+++ nlp100.py
@@ -11,6 +11,24 @@ str = "stressed"
 print str[::-1]
 
 
+# ## No.2
+
+# In[ ]:
+
+str = u"パタトクカシーー"
+print str[::2]
+
+
+# ## No.3
+
+# In[ ]:
+
+str1 = u"パトカー"
+str2 = u"タクシー"
+result = "".join([a + b for a, b in zip(str1, str2)])
+print result
+
+
 # In[ ]:
 
 
